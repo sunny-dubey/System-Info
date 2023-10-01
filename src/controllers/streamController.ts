025721c2bf1c569f9ee641dfb35910eed53e6aed
Sunny Dubey <sunnydubey2502@gmail.com>
2023-10-01 14:13:10 +0530

    add stream type in create stream

--- src/controllers/streamController.ts
+++ src/controllers/streamController.ts
@@ -2,16 +2,23 @@ import express, { Request, Response, NextFunction } from 'express';
 import {fetchCpuUsage, fetchMemoryUsage} from "../utils/systemInfo"
 import AppError from '../utils/appError';
 
+// enum for stream types
+enum StreamType{
+  cpu = 'cpu',
+  memory = 'memory'
+}
+
 interface StreamInfo{
   //key: string;
   isRunning: boolean;
+  streamType: StreamType;
 }
 
 const activeStreams: {[Key: string]:StreamInfo}= {};
 
 export const createStream = (req: Request, res: Response, next: NextFunction): void => {
     let {type} = req.params;
-    type = type.toLocaleLowerCase();
+    type = type.toLowerCase();
     if(type!='cpu' && type!='memory'){
       return next(new AppError('Please enter the correct stream type', 400));
     }
@@ -20,10 +27,12 @@ export const createStream = (req: Request, res: Response, next: NextFunction): v
     // Initialize the stream information
     activeStreams[streamId] = {
     isRunning: false,
+    streamType: type as StreamType,
     };
     //console.log(activeStreams[streamId]);
     res.status(200).json({
       streamId: streamId,
+      StreamType: type,
       message: "Stream created SuccessfullyQ"
     })
 };
